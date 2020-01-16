8646d11e980e36518a136a26033d7b5e51808ca6
antonioagostino <antony_97-@hotmail.it>
2020-01-16 18:15:08 +0100

    added connection to a remote PostgreSQL database

--- WEBProject/src/persistence/DBManager.java
+++ WEBProject/src/persistence/DBManager.java
@@ -20,7 +20,7 @@ public class DBManager {
 			Class.forName("org.postgresql.Driver").newInstance();
 			//questi vanno messi in file di configurazione!!!	
 //			dataSource=new DataSource("jdbc:postgresql://52.39.164.176:5432/xx","xx","p@xx");
-			dataSource = new DataSource("jdbc:postgresql://localhost:5432/technoworld","postgres","admin");
+			dataSource = new DataSource("jdbc:postgresql://manny.db.elephantsql.com:5432/ndbzhnht","ndbzhnht","orWOykJIylSTUE8z1z2yXSFfvAn1jyIH");
 		} 
 		catch (Exception e) {
 			System.err.println("PostgresDAOFactory.class: failed to load MySQL JDBC driver\n"+e);
