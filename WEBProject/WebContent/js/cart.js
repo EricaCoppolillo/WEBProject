dad2a2cabcaf06ea920bc6b243edbeeee01b44a9
Erica Boh <Erica Boh@EricaBoh.homenet.telecomitalia.it>
2020-01-23 15:29:09 +0100

    fixed bugs in cart page and added functionalities

--- WEBProject/WebContent/js/cart.js
+++ WEBProject/WebContent/js/cart.js
@@ -9,4 +9,39 @@ function removeCartProduct(idProduct) {
 		data: {idProduct: idProduct},
 		success: function(data) {}
 	});
+	
+	$.ajax({
+		type: "GET",
+		url: "updateTotalPrice",
+		success: function(data) {
+			var newTotPrice = parseFloat(data);
+			document.getElementById("totalPrice").innerHTML = newTotPrice.toFixed(2) + " €";
+		}
+	});
+}
+
+function updateFunctions(idProduct) {
+	
+	var selectProduct = "#productQuantity"+idProduct;
+	var newQuantity = $(selectProduct).children("option:selected").val();
+	$.ajax({
+		type: "GET",
+		url: "updateQuantity",
+		data: {idProduct: idProduct, newQuantity: newQuantity},
+		success: function(data) {
+			var resp = data.split(" ");
+				
+			var divPrice = "productPrice"+idProduct;
+			document.getElementById(divPrice).innerHTML = parseFloat(resp[0]).toFixed(2) + " €";
+			$("#"+divPrice).css('color', '#c10d0d');
+			
+			var newTotPrice = parseFloat(resp[1]);
+			document.getElementById("totalPrice").innerHTML = newTotPrice.toFixed(2) + " €";
+		}
+	});
+}
+
+function updateTotalPrice() {
+
+	
 }
