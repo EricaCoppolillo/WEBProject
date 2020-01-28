02126635f613f9335669bdae41d2ec7291a1f9c6
antonioagostino <antony_97-@hotmail.it>
2020-01-28 15:53:20 +0100

    fixed pagination problem and payment's bugs

--- WEBProject/src/controller/CharsetFilter.java
+++ WEBProject/src/controller/CharsetFilter.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class CharsetFilter implements Filter {
@@ -12,10 +13,18 @@ public class CharsetFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) throws IOException, ServletException {
-        request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        next.doFilter(request, response);
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getRequestURI();
+
+        if(!(path.endsWith(".css") || path.endsWith(".js") || path.endsWith(".jpg") || path.endsWith(".png"))){
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html; charset=UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            next.doFilter(request, response);
+        } else {
+            next.doFilter(request, response);
+        }
     }
 
     @Override
